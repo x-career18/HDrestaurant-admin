76da7259d1bdb7542e17a65f8a9f0f12f82c08b0
hija971 <hieupham.211197@gmail.com>
2023-11-06 01:19:29 +0700

    update

--- src/context/authContext/apiCalls.jsx
+++ src/context/authContext/apiCalls.jsx
@@ -7,15 +7,16 @@ export const login = async (user, dispatch) => {
   try {
     const res = await axios.post("api/v1/auth/login", user);
     console.log(res.data);
+    axios.defaults.headers.common[
+      "Authorization"
+    ] = `Bearer ${res.data.accessToken}`;
     if (res.data.accessToken) {
       const decodedToken = jwtDecode(res.data.accessToken);
-      axios.defaults.headers.common["Authorization"] = `Bearer ${res.data.accessToken}`;
       dispatch(loginSuccess(decodedToken));
       localStorage.setItem("accessToken", res.data.accessToken);
-      axios.defaults.headers.common["Authorization"] = `Bearer ${res.data.accessToken}`;
     }
   } catch (error) {
-    dispatch(loginFailure())
+    dispatch(loginFailure());
     if (error.response && error.response.data && error.response.data.message) {
       console.log(error.response.data.message);
       throw new Error(error.response.data.message); // Throw the error to be caught later
