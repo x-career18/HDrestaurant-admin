86e2c9d982b81618e1e6e33f24c3e6a2b31fd44f
Pupa06 <126087025+Pupa06@users.noreply.github.com>
2023-11-06 01:38:59 +0700

    update restaurant list

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api/v1': {
-        target: 'https://hd-restaurant-be.onrender.com/',
-        changeOrigin: true,
+        target: 'http://localhost:5000',
+        // changeOrigin: true,
       },
     },
   },
