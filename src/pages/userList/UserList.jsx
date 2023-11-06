3672521ec83b2f844107bce7e6d34e148df06f56
Pupa06 <126087025+Pupa06@users.noreply.github.com>
2023-11-07 02:10:06 +0700

    update ghép api

--- src/pages/userList/UserList.jsx
+++ src/pages/userList/UserList.jsx
@@ -1,44 +1,121 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import BreadCrumb from '../../components/BreadCrumb'
-import ListItem from '../../components/ListItem'
+import { Button, Table, message } from 'antd'
+import moment from 'moment';
+import { fetchUserManager, fetchUserUpdate, fetchUserDelete } from '../../services/UserSevices.jsx'
+import './UserList.css'
 
 const UserList = () => {
-  const numColumns = 6
-  const numRows = 10
+  const columns = [
+    {
+      title: 'Tên Người Dùng',
+      dataIndex: 'fullname',
+      key: 'fullname',
+    },
+    {
+      title: 'Email',
+      dataIndex: 'email',
+      key: 'email',
+    },
+    {
+      title: 'Số Điện Thoại',
+      dataIndex: 'phonenumber',
+      key: 'phonenumber',
+    },
+    {
+      title: 'Ngày Tạo',
+      dataIndex: 'createdAt',
+      key: 'createdAt',
+      render: (text, record) => moment(text).format('DD/MM/YYYY')
+    },
+    {
+      title: 'Trạng Thái',
+      dataIndex: 'isActive',
+      key: 'isActive',
+      render: (isActive, record) => (
+        <div>
+          <Button
+            type="primary"
+            onClick={() => handleConfirm(record)}
+            disabled={isActive}
+          >
+            Xác nhận
+          </Button>
+          <Button
+            type="danger"
+            onClick={() => handleDelete(record)}
+            disabled={isActive}
+            style={{ backgroundColor: '#FF3B3B1A', borderColor: '#E92C2C', marginLeft: 10, color: '#E92C2C' }}
+          >
+            Xóa
+          </Button>
+        </div>
+      ),
+    },
+  ]
+
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  const getManager = async () => {
+    setLoading(true)
+    try {
+      const response = await fetchUserManager()
+      if (response && response.data) {
+        setData(response.data);
+      }
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    getManager()
+  }, [])
+
+  const handleConfirm = async (record) => {
+    try {
+      const response = await fetchUserUpdate(record._id, { isActive: true });
+      if (response && response.data) {
+        getManager();
+      }
+    } catch (error) {
+      console.log(error)
+      message.error("Xác nhận thất bại");
+    }
+  }
+
+  const handleDelete = async (record) => {
+    try {
+      const response = await fetchUserDelete(record._id);
+      if (response && response.data) {
+        getManager();
+      }
+    } catch (error) {
+      console.log(error)
+      message.error("Xóa thất bại");
+    }
+  }
+
   return (
     <main className="bg-slate-100 grow h-screen flex flex-col">
-    <BreadCrumb />
-    <section className="flex flex-col pb-10 mx-10 mb-10">
-      <h4 className="text-neutral-600 text-xl font-bold font-beVietnam leading-10">
-        List of Restaurants
-      </h4>
-      <table className="table-auto">
-        <thead>
-          <tr className="h-14">
-            <th className="text-neutral-600 text-lg font-bold font-beVietnam leading-10 tracking-tight">
-              No.
-            </th>
-            <th className="text-neutral-600 text-lg font-bold font-beVietnam leading-loose">
-              User's name
-            </th>
-            <th className="text-neutral-600 text-lg font-bold font-beVietnam leading-loose">
-              Creator
-            </th>
-            <th className="text-neutral-600 text-lg font-bold font-beVietnam leading-loose">
-              Restaurant
-            </th>
-            <th className="text-neutral-600 text-lg font-bold font-beVietnam leading-loose">
-              ID
-            </th>
-            <th className="text-neutral-600 text-lg font-bold font-beVietnam leading-loose">
-              Status
-            </th>
-          </tr>
-        </thead>
-        <ListItem numColumns={numColumns} numRows={numRows} />
-      </table>
-    </section>
-  </main>
+      <BreadCrumb />
+      <section className="flex flex-col pb-10 mx-10 mb-10">
+        <h4 className="text-neutral-600 text-xl font-bold font-beVietnam leading-10">
+          Danh sách quản lý
+        </h4>
+        <div style={{ width: '100%', height: '100%' }}>
+          <Table
+            className="main-table"
+            columns={columns}
+            dataSource={data}
+            loading={loading}
+          />
+        </div>
+      </section>
+    </main>
   )
 }
 
