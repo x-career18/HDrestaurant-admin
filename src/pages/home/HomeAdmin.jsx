59bef212cffc41b36f82e8947d54723003bc6a84
hija971 <hieupham.211197@gmail.com>
2023-11-09 00:52:47 +0700

    update homeAdmin

--- src/pages/home/HomeAdmin.jsx
+++ src/pages/home/HomeAdmin.jsx
@@ -8,6 +8,8 @@ import {
   fetchNewEmployee,
 } from "../../services/UserSevices";
 import { fetchRestaurants } from "../../services/RestaurantServices";
+import { Table } from "antd";
+import moment from "moment";
 
 const HomeAdmin = () => {
   const [numManagers, setNumManagers] = useState([]);
@@ -15,6 +17,8 @@ const HomeAdmin = () => {
   const [numNewEmployees, setNumNewEmployees] = useState([]);
   const [numNewManagers, setNumNewManagers] = useState([]);
   const [numRestaurants, setNumRestaurants] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
   const getManager = async () => {
     try {
       const response = await fetchUserManager();
@@ -58,25 +62,87 @@ const HomeAdmin = () => {
   };
 
   const getRestaurants = async () => {
+    setLoading(true);
     try {
       const res = await fetchRestaurants();
       if (res && res.data) {
+        const sortedData = res.data.sort(
+          (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
+        );
         setNumRestaurants(res.data);
+        setData(sortedData);
       }
     } catch (error) {
       console.log(error);
+    } finally {
+      setLoading(false);
     }
   };
 
   useEffect(() => {
     getManager();
     getEmployee();
-    getNewManager(), getNewEmployee(), getRestaurants();
+    getNewManager();
+    getNewEmployee();
+    getRestaurants();
   }, []);
 
   const allUsers = numManagers.length + numEmployees.length;
   const allNewUsers = numNewManagers.length + numNewEmployees.length;
   const allRestaurants = numRestaurants.length;
+
+  const columns = [
+    {
+      width: 100,
+      title: " ",
+      dataIndex: "image",
+      key: "image",
+      render: (avatar) => (
+        <img
+          src={avatar || DEFAULT_IMAGE}
+          alt="avatar"
+          style={{ width: 50, height: 32, borderRadius: 4 }}
+          onError={(e) => {
+            e.target.src = DEFAULT_IMAGE;
+          }}
+        />
+      ),
+    },
+    {
+      title: "Tên nhà hàng",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Ngày tạo",
+      dataIndex: "createdAt",
+      key: "createdAt",
+      render: (text) => moment(text).format("DD/MM/YYYY"),
+    },
+    {
+      width: 400,
+      title: "Restaurant ID",
+      dataIndex: "idRestaurant",
+      key: "idRestaurant",
+    },
+    {
+      width: 200,
+      title: "Trạng thái",
+      dataIndex: "isVerified",
+      key: "isVerified",
+      render: (isVerified) => (
+        <div className="flex items-center gap-10">
+          <span
+            className={`w-16 text-center text-white p-1 rounded-3xl ${
+              isVerified ? "bg-green-500" : "bg-rose-500"
+            }`}
+          >
+            {isVerified ? "Verified" : "Pending"}
+          </span>
+        </div>
+      ),
+    },
+  ];
 
   return (
     <div className="bg-slate-100 grow h-screen flex flex-col">
@@ -92,6 +158,17 @@ const HomeAdmin = () => {
         thirdText={"nhà hàng"}
         thirdTitle={"Tổng số nhà hàng"}
       />
+      <div className="mx-12 mt-12">
+        <h5 className="font-bold text-lg mb-5 font-beVietnam">
+          Nhà hàng đã tạo gần đây
+        </h5>
+        <Table
+          className="bg-white font-semibold text-sm leading-5 text-[#969696] drop-shadow-[0_3px_10px_rgba(119,119,119,0.10)]"
+          columns={columns}
+          dataSource={data}
+          loading={loading}
+        />
+      </div>
     </div>
   );
 };
