5693909838788dec1df10ed22b9eea962fef232e
Pupa06 <126087025+Pupa06@users.noreply.github.com>
2023-11-20 16:08:26 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
     plugins: [react()],
     server: {
+        open: true,
         proxy: {
             '/api': {
                 target: 'https://hd-restaurant-be.onrender.com/',
@@ -15,3 +16,4 @@ export default defineConfig({
         },
     },
 })
+
