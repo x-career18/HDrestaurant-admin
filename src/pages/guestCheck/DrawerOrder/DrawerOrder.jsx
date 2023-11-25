48613296d0a2fb7280980b21b5dbc44190e7a1ac
Tran Xuan Son <tranxuanson98@gmail.com>
2023-11-25 23:12:57 +0700

    edit able row

--- src/pages/guestCheck/DrawerOrder/DrawerOrder.jsx
+++ src/pages/guestCheck/DrawerOrder/DrawerOrder.jsx
@@ -1,217 +1,388 @@
-import { Button, Checkbox, Drawer, Form, Input, InputNumber, Modal, Table } from "antd";
-import { useEffect, useState } from "react";
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import {
+  Button,
+  Checkbox,
+  Drawer,
+  Form,
+  Input,
+  InputNumber,
+  Modal,
+  Table,
+} from "antd";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import { fetchMenus } from "../../../services/MenuServices";
 
 const columnsMenuOrder = [
-    {
-        width: 100,
-        title: 'Tên món',
-        dataIndex: 'dishName',
-        key: 'dishName',
-    },
-    {
-        title: 'Loại món',
-        dataIndex: 'category',
-        key: 'category',
-    },
-    {
-        title: 'Số lượng',
-        dataIndex: 'quantity',
-        key: 'quantity',
-    },
-    {
-        title: 'Giá',
-        dataIndex: 'total',
-        key: 'total',
-        render: (text, record) => (
-            <span>
-                {record.price && typeof record.price === 'number'
-                    ? record.price.toLocaleString('vi-VN', { style: 'currency', currency: 'VND' })
-                    : record.price}
-            </span>
-        ),
-    },
+  {
+    width: 100,
+    title: "Tên món",
+    dataIndex: "name",
+    key: "name",
+  },
+  {
+    title: "Loại món",
+    dataIndex: "category",
+    key: "category",
+  },
+  {
+    title: "Số lượng",
+    dataIndex: "quantity",
+    key: "quantity",
+  },
+  {
+    title: "Giá",
+    dataIndex: "total",
+    key: "total",
+    render: (text, record) => (
+      <span>
+        {record.total && typeof record.total === "number"
+          ? record.total.toLocaleString("vi-VN", {
+              style: "currency",
+              currency: "VND",
+            })
+          : record.total}
+      </span>
+    ),
+  },
 ];
 
 const columnsMenu = [
-    {
-        title: '',
-        key: 'select',
-        width: 50,
-        render: (record) => (
-            <Checkbox />
-        ),
-    },
-    {
-        title: 'Tên món',
-        dataIndex: 'name',
-        key: 'name',
-    },
-    {
-        title: 'Loại món',
-        dataIndex: 'category',
-        key: 'category',
-    },
-    {
-        title: 'Số lượng',
-        dataIndex: 'quantity',
-        key: 'quantity',
-        render: (text, record) => (
-            <InputNumber
-                min={1}
-                defaultValue={1}
-            />
-        )
-    },
-    {
-        title: 'Giá',
-        dataIndex: 'price',
-        key: 'price',
-        render: (text, record) => (
-            <span>
-                {record.price && typeof record.price === 'number'
-                    ? record.price.toLocaleString('vi-VN', { style: 'currency', currency: 'VND' })
-                    : record.price}
-            </span>
-        ),
-    },
+  {
+    title: "Tên món",
+    dataIndex: "name",
+    key: "name",
+  },
+  {
+    title: "Loại món",
+    dataIndex: "category",
+    key: "category",
+  },
+  {
+    title: "Số lượng",
+    dataIndex: "quantity",
+    key: "quantity",
+    editable: true,
+    render: (number) => (number > 0 ? number : "-"),
+  },
+  {
+    title: "Giá",
+    dataIndex: "price",
+    key: "price",
+    render: (text, record) => (
+      <span>
+        {record.price && typeof record.price === "number"
+          ? record.price.toLocaleString("vi-VN", {
+              style: "currency",
+              currency: "VND",
+            })
+          : record.price}
+      </span>
+    ),
+  },
 ];
 
-
 function DrawerOrder(props) {
-    const { isOpenOrder, closeOrder, selectedBooking } = props;
+  const { isOpenOrder, closeOrder, selectedBooking } = props;
 
+  const [form] = Form.useForm();
+  const [isModalMenu, setIsModalMenu] = useState(false);
+  const [dataMenu, setDataMenu] = useState([]);
+  const [selectedProductKeys, setSelectedProductKeys] = useState([]);
+  const [tableData, setTableData] = useState([]);
+
+  const openModalMenu = () => {
+    setIsModalMenu(true);
+  };
+
+  const closeModalMenu = () => {
+    setIsModalMenu(false);
+  };
+
+  useEffect(() => {
+    if (selectedBooking) {
+      form.setFieldsValue(selectedBooking);
+    }
+  }, [selectedBooking]);
+
+  const fetchMenuData = async () => {
+    try {
+      const response = await fetchMenus();
+      if (response && response.data) {
+        console.log(response?.data, 123123);
+        const mappingData = (response?.data || []).map((item) => ({
+          ...item,
+          key: item?._id,
+        }));
+        setDataMenu(mappingData);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchMenuData();
+  }, []);
+
+  const EditableRow = ({ index, ...propsRow }) => {
     const [form] = Form.useForm();
-    const [isModalMenu, setIsModalMenu] = useState(false);
-    const [dataMenu, setDataMenu] = useState([]);
-
-    const openModalMenu = () => {
-        setIsModalMenu(true);
-    };
-
-    const closeModalMenu = () => {
-        setIsModalMenu(false);
-    };
-
-    useEffect(() => {
-        if (selectedBooking) {
-            form.setFieldsValue(selectedBooking);
-        }
-    }, [selectedBooking])
-
-    const fetchMenuData = async () => {
-        try {
-            const response = await fetchMenus();
-            if (response && response.data) {
-                setDataMenu(response.data);
-            }
-        } catch (error) {
-            console.log(error);
-        }
-    };
-
-    useEffect(() => {
-        fetchMenuData();
-    }, []);
-
-
     return (
-        <div>
-            <Drawer
-                width={650}
-                title="Thông tin order của khách hàng"
-                open={isOpenOrder}
-                onClose={closeOrder}
-            >
-                <Form
-                    form={form}
-                    layout="vertical"
-                    initialValues={selectedBooking}
-                >
-                    <div>
-                        <div className="form-divide">
-                            <div className="child-form">
-                                <Form.Item
-                                    label="Tên khách hàng"
-                                    name="fullName"
-                                >
-                                    <Input disabled />
-                                </Form.Item>
-                            </div>
-                            <div className="child-form">
-                                <Form.Item
-                                    label="Số điện thoại"
-                                    name="phoneNumber"
-                                >
-                                    <Input disabled />
-                                </Form.Item>
-                            </div>
-                        </div>
-                        <div className="form-divide">
-                            <div className="child-form">
-                                <Form.Item
-                                    label="Mã nhân viên"
-                                    name="employeeCode"
-                                    initialValue={
-                                        localStorage.getItem("user")
-                                            ? JSON.parse(localStorage.getItem("user")).employeeCode
-                                            : ""
-                                    }
-                                >
-                                    <Input disabled />
-                                </Form.Item>
-                            </div>
-                        </div>
-                        <div className="form-divide">
-                            <div className="child-form">
-                                <Form.Item
-                                    label="Món ăn"
-                                    name="dishes"
-                                >
-                                    <Button
-                                        type="primary"
-                                        htmlType="submit"
-                                        style={{
-                                            backgroundColor: "#35B968",
-                                            borderColor: "#35B968",
-                                            color: "#FFF",
-                                        }}
-                                        onClick={openModalMenu}
-                                    >
-                                        Thêm món ăn
-                                    </Button>
-                                </Form.Item>
-                            </div>
-                        </div>
-                    </div>
-                </Form>
-                <Table
-                    className="main-table"
-                    columns={columnsMenuOrder}
-                />
-            </Drawer>
-            <Modal
-                title="Danh sách món ăn"
-                width={600}
-                open={isModalMenu}
-                onCancel={closeModalMenu}
-                okText="Xác nhận"
-                cancelText="Hủy"
-                centered
-                className="modal-create"
-            >
-                <div>
-                    <Table
-                        className="main-table"
-                        columns={columnsMenu}
-                        dataSource={dataMenu}
-                        pagination={false}
-                    />
-                </div>
-            </Modal>
-        </div>
-
+      <Form form={form} component={false}>
+        <EditableContext.Provider value={form}>
+          <tr {...propsRow} />
+        </EditableContext.Provider>
+      </Form>
     );
+  };
+
+  const EditableCell = ({
+    title,
+    editable,
+    children,
+    dataIndex,
+    record,
+    handleSave,
+    ...restProps
+  }) => {
+    const [editing, setEditing] = useState(false);
+    const inputRef = useRef(null);
+    const form = useContext(EditableContext);
+
+    useEffect(() => {
+      if (editing) {
+        inputRef.current.focus();
+      }
+    }, [editing]);
+
+    const toggleEdit = () => {
+      setEditing(!editing);
+      form.setFieldsValue({ [dataIndex]: record[dataIndex] });
+    };
+
+    const save = async () => {
+      try {
+        const values = await form.validateFields();
+        toggleEdit();
+        handleSave({ ...record, ...values });
+      } catch (errInfo) {
+        console.log("Save failed:", errInfo);
+      }
+    };
+
+    let childNode = children;
+
+    if (editable) {
+      childNode = editing ? (
+        <Form.Item
+          style={{ margin: 0 }}
+          name={dataIndex}
+          rules={[
+            {
+              required: true,
+              message: `Vui lòng nhập ${title}.`,
+            },
+            {
+              type: "number",
+              min: 1,
+              max: +record?.quantity,
+              message: `Vui lòng nhập số lượng từ 1 đến ${record?.quantity}`,
+            },
+          ]}
+        >
+          <InputNumber
+            ref={inputRef}
+            // min={1}
+            // max={record?.quantity}
+            onPressEnter={save}
+            onBlur={save}
+          />
+        </Form.Item>
+      ) : (
+        <div
+          className={
+            selectedProductKeys.includes(record?.key) &&
+            "editable-cell-value-wrap"
+          }
+          style={{ paddingRight: 24 }}
+          onClick={selectedProductKeys.includes(record?.key) && toggleEdit}
+        >
+          {children}
+        </div>
+      );
+    }
+
+    return <td {...restProps}>{childNode}</td>;
+  };
+
+  const components = {
+    body: {
+      row: EditableRow,
+      cell: EditableCell,
+    },
+  };
+
+  const EditableContext = React.createContext(null);
+
+  const editColumns = columnsMenu.map((col) => {
+    if (!col.editable) {
+      return col;
+    }
+    return {
+      ...col,
+      onCell: (record) => ({
+        record,
+        editable: col.editable,
+        dataIndex: col.dataIndex,
+        title: col.title,
+        handleSave,
+      }),
+    };
+  });
+
+  const handleSave = (row) => {
+    const newData = [...dataMenu];
+    const index = newData.findIndex((item) => row.key === item.key);
+    const item = newData[index];
+    newData.splice(index, 1, {
+      ...item,
+      ...row,
+    });
+    setDataMenu(newData);
+    setTableData(
+      newData
+        .filter((product) => selectedProductKeys.includes(product?._id))
+        .map((item) => {
+          return {
+            ...item,
+            total: item?.quantity * item?.price,
+          };
+        })
+    );
+  };
+
+  useEffect(() => {
+    console.log("table Data", tableData);
+    console.log("dataMenu", dataMenu);
+  }, [tableData, dataMenu]);
+
+  return (
+    <div>
+      <Drawer
+        width={650}
+        title="Thông tin order của khách hàng"
+        open={isOpenOrder}
+        onClose={closeOrder}
+      >
+        <Form form={form} layout="vertical" initialValues={selectedBooking}>
+          <div>
+            <div className="form-divide">
+              <div className="child-form">
+                <Form.Item label="Tên khách hàng" name="fullName">
+                  <Input disabled />
+                </Form.Item>
+              </div>
+              <div className="child-form">
+                <Form.Item label="Số điện thoại" name="phoneNumber">
+                  <Input disabled />
+                </Form.Item>
+              </div>
+            </div>
+            <div className="form-divide">
+              <div className="child-form">
+                <Form.Item
+                  label="Mã nhân viên"
+                  name="employeeCode"
+                  initialValue={
+                    localStorage.getItem("user")
+                      ? JSON.parse(localStorage.getItem("user")).employeeCode
+                      : ""
+                  }
+                >
+                  <Input disabled />
+                </Form.Item>
+              </div>
+            </div>
+            <div className="form-divide">
+              <div className="child-form">
+                <Form.Item label="Món ăn" name="dishes">
+                  <Button
+                    type="primary"
+                    htmlType="submit"
+                    style={{
+                      backgroundColor: "#35B968",
+                      borderColor: "#35B968",
+                      color: "#FFF",
+                    }}
+                    onClick={openModalMenu}
+                  >
+                    Thêm món ăn
+                  </Button>
+                </Form.Item>
+              </div>
+            </div>
+          </div>
+        </Form>
+        <Table
+          className="main-table"
+          columns={columnsMenuOrder}
+          dataSource={tableData}
+        />
+      </Drawer>
+      <Modal
+        title="Danh sách món ăn"
+        width={600}
+        open={isModalMenu}
+        onCancel={closeModalMenu}
+        onOk={closeModalMenu}
+        okText="Xác nhận"
+        cancelText="Hủy"
+        centered
+        className="modal-create"
+      >
+        <div>
+          <Table
+            className="main-table"
+            // columns={columnsMenu}
+            columns={editColumns}
+            dataSource={dataMenu}
+            pagination={false}
+            components={components}
+            rowSelection={{
+              columnWidth: 52,
+              hideSelectAll: true,
+              selectedRowKeys: selectedProductKeys,
+              onChange: (newSelectedRowKeys) => {
+                setSelectedProductKeys(newSelectedRowKeys);
+                setTableData(
+                  dataMenu
+                    .filter((product) =>
+                      newSelectedRowKeys.includes(product?._id)
+                    )
+                    .map((item) => {
+                      return {
+                        ...item,
+                        total: item?.quantity * item?.price,
+                      };
+                    })
+                );
+              },
+              onSelect: (record, selected) => {
+                if (!selected) {
+                  handleSave({
+                    ...record,
+                    accessory_num: 0,
+                  });
+                }
+              },
+            }}
+            rowKey="key"
+            rowClassName={() => "editable-row"}
+          />
+        </div>
+      </Modal>
+    </div>
+  );
 }
 
 export default DrawerOrder;
--- src/pages/guestCheck/GuestCheck.jsx
+++ src/pages/guestCheck/GuestCheck.jsx
@@ -1,143 +1,155 @@
-import React, { useEffect, useState } from 'react'
-import BreadCrumb from '../../components/BreadCrumb'
-import moment from 'moment';
-import { Button, Form, Modal, Table } from 'antd';
-import { SettingOutlined } from '@ant-design/icons';
-import { fetchBookings } from '../../services/BookingServices';
-import './GuestCheck.scss'
-import DrawerOrder from './DrawerOrder/DrawerOrder';
+import { useEffect, useState } from "react";
+import BreadCrumb from "../../components/BreadCrumb";
+import moment from "moment";
+import { Button, Form, Table } from "antd";
+import { SettingOutlined } from "@ant-design/icons";
+import { fetchBookings } from "../../services/BookingServices";
+import "./GuestCheck.scss";
+import DrawerOrder from "./DrawerOrder/DrawerOrder";
 
 function GuestCheck() {
+  const columns = [
+    {
+      width: 150,
+      title: "Tên khách hàng",
+      dataIndex: "fullName",
+      key: "fullName",
+    },
+    {
+      title: "Số điện thoại",
+      dataIndex: "phoneNumber",
+      key: "phoneNumber",
+    },
+    {
+      title: "Ngày đặt bàn",
+      dataIndex: "bookingDate",
+      key: "bookingDate",
+      render: (text) => moment(text).format("DD/MM/YYYY"),
+    },
+    {
+      title: "Giờ đặt",
+      dataIndex: "bookingTime",
+      key: "bookingTime",
+    },
+    {
+      title: "Số lượng khách",
+      dataIndex: "numberOfPeople",
+      key: "numberOfPeople",
+    },
+    {
+      title: "Ghi chú",
+      dataIndex: "message",
+      key: "message",
+    },
+    {
+      width: 100,
+      title: "Trạng thái",
+      dataIndex: "status",
+      key: "status",
+      render: (status) => (
+        <div className="flex items-center gap-10">
+          <span
+            className={`w-20 text-center text-white p-1 rounded-3xl ${
+              status === "pending" ? "bg-rose-500" : "bg-green-500"
+            }`}
+          >
+            {status === "pending" ? "Pending" : "Active"}
+          </span>
+        </div>
+      ),
+    },
+    {
+      width: 100,
+      title: "Thao tác",
+      dataIndex: "status",
+      key: "status",
+      render: (status, record) => (
+        <div style={{ display: "flex", alignItems: "center", gap: 10 }}>
+          <Button
+            onClick={() => openOrder(record)}
+            style={{
+              border: "#6A707E",
+              backgroundColor: "#FFF",
+              color: "#6A707E",
+              padding: "0 10px",
+              display: "flex",
+              alignItems: "center",
+              gap: 5,
+            }}
+          >
+            <SettingOutlined />
+          </Button>
+          <Button
+            type="danger"
+            style={{
+              backgroundColor: "#FF3B3B1A",
+              borderColor: "#E92C2C",
+              color: "#E92C2C",
+              padding: "0 10px",
+            }}
+          >
+            Xóa
+          </Button>
+        </div>
+      ),
+    },
+  ];
 
-    const columns = [
-        {
-            width: 150,
-            title: 'Tên khách hàng',
-            dataIndex: 'fullName',
-            key: 'fullName',
-        },
-        {
-            title: 'Số điện thoại',
-            dataIndex: 'phoneNumber',
-            key: 'phoneNumber',
-        },
-        {
-            title: 'Ngày đặt bàn',
-            dataIndex: 'bookingDate',
-            key: 'bookingDate',
-            render: (text) => moment(text).format("DD/MM/YYYY"),
-        },
-        {
-            title: 'Giờ đặt',
-            dataIndex: 'bookingTime',
-            key: 'bookingTime',
-        },
-        {
-            title: 'Số lượng khách',
-            dataIndex: 'numberOfPeople',
-            key: 'numberOfPeople',
-        },
-        {
-            title: 'Ghi chú',
-            dataIndex: 'message',
-            key: 'message',
-        },
-        {
-            width: 100,
-            title: 'Trạng thái',
-            dataIndex: 'status',
-            key: 'status',
-            render: (status) => (
-                <div className="flex items-center gap-10">
-                    <span
-                        className={`w-20 text-center text-white p-1 rounded-3xl ${status === "pending" ? "bg-rose-500" : "bg-green-500"}`}
-                    >
-                        {status === "pending" ? "Pending" : "Active"}
-                    </span>
-                </div>
-            ),
-        },
-        {
-            width: 100,
-            title: 'Thao tác',
-            dataIndex: 'status',
-            key: 'status',
-            render: (status, record) => (
-                <div style={{ display: 'flex', alignItems: 'center', gap: 10 }}>
-                    <Button
-                        onClick={() => openOrder(record)}
-                        style={{ border: '#6A707E', backgroundColor: '#FFF', color: '#6A707E', padding: '0 10px', display: 'flex', alignItems: 'center', gap: 5 }}
-                    >
-                        <SettingOutlined />
-                    </Button>
-                    <Button
-                        type="danger"
-                        style={{ backgroundColor: '#FF3B3B1A', borderColor: '#E92C2C', color: '#E92C2C', padding: '0 10px' }}
-                    >
-                        Xóa
-                    </Button>
-                </div>
-            )
-        },
+  const [latestBookings, setLatestBookings] = useState([]);
+  const [isOpenOrder, setIsOpenOrder] = useState(false);
+  const [selectedBooking, setSelectedBooking] = useState(null);
+  const [form] = Form.useForm();
 
-    ];
+  const openOrder = (record) => {
+    setSelectedBooking(record);
+    setIsOpenOrder(true);
+  };
 
+  const closeOrder = () => {
+    setIsOpenOrder(false);
+  };
 
-    const [latestBookings, setLatestBookings] = useState([]);
-    const [isOpenOrder, setIsOpenOrder] = useState(false);
-    const [selectedBooking, setSelectedBooking] = useState(null);
-    const [form] = Form.useForm();
+  const fetchData = async () => {
+    try {
+      const res = await fetchBookings();
+      const employee = JSON.parse(localStorage.getItem("user"));
+      const matchingBookings = res.data.filter(
+        (booking) =>
+          booking.restaurantId === employee.restaurantId &&
+          booking.status === "active"
+      );
 
-    const openOrder = (record) => {
-        setSelectedBooking(record);
-        setIsOpenOrder(true);
-    };
-
-    const closeOrder = () => {
-        setIsOpenOrder(false);
-    };
-
-    const fetchData = async () => {
-        try {
-            const res = await fetchBookings();
-            const employee = JSON.parse(localStorage.getItem("user"));
-            const matchingBookings = res.data.filter(
-                (booking) => booking.restaurantId === employee.restaurantId && booking.status === "active"
-            );
-
-            setLatestBookings(matchingBookings);
-
-        } catch (error) {
-            console.log(error);
-        }
+      setLatestBookings(matchingBookings);
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    useEffect(() => {
-        fetchData();
-    }, []);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-
-    return (
-        <main className="bg-slate-100 grow h-screen flex flex-col">
-            <BreadCrumb />
-            <section className="flex flex-col pb-10 mx-10 mb-10">
-                <h4 className="text-neutral-600 text-xl font-bold font-beVietnam leading-10">
-                    Check book
-                </h4>
-                <Table
-                    className="main-table"
-                    columns={columns}
-                    dataSource={latestBookings}
-                />
-            </section>
-            <DrawerOrder 
-                isOpenOrder={isOpenOrder}
-                closeOrder={closeOrder}
-                form={form}
-                selectedBooking={selectedBooking}
-            />
-        </main>
-    )
+  return (
+    <main className="bg-slate-100 grow h-screen flex flex-col">
+      <BreadCrumb />
+      <section className="flex flex-col pb-10 mx-10 mb-10">
+        <h4 className="text-neutral-600 text-xl font-bold font-beVietnam leading-10">
+          Check book
+        </h4>
+        <Table
+          className="main-table"
+          columns={columns}
+          dataSource={latestBookings}
+        />
+      </section>
+      <DrawerOrder
+        isOpenOrder={isOpenOrder}
+        closeOrder={closeOrder}
+        form={form}
+        selectedBooking={selectedBooking}
+      />
+    </main>
+  );
 }
 
-export default GuestCheck
+export default GuestCheck;
